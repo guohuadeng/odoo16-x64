7feb04f331744fde0b8f15a93cd15240355ddca5
ivan deng <guohuadeng@hotmail.com>
2023-02-21 21:19:51 +0800

    fix debug

--- source/odoo/tools/translate.py
+++ source/odoo/tools/translate.py
@@ -1445,6 +1445,8 @@ def resetlocale():
     # locale.resetlocale is bugged with some locales.
     for ln in get_locales():
         try:
+            if ln.find('.') >= 0:
+                ln = ln[0:ln.index('.')]
             return locale.setlocale(locale.LC_ALL, ln)
         except locale.Error:
             continue
